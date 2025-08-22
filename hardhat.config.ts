fac1b955c512c7e19242a66ba724665747f8fea1
ChainUniverse <39116331+ChainUniverse@users.noreply.github.com>
2025-08-22 19:57:04 +0800

    deploy to sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,14 +8,20 @@ import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
 import { vars } from "hardhat/config";
 import "solidity-coverage";
+import * as dotenv from "dotenv";
 
 import "./tasks/accounts";
 import "./tasks/FHECounter";
+import "./tasks/PredictionMarket";
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Run 'npx hardhat vars setup' to see the list of variables that need to be set
 
 const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY || "";
+const INFURA_API_KEY: string = process.env.INFURA_API_KEY || vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -24,7 +30,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      sepolia: vars.get("ETHERSCAN_API_KEY", ""),
+      sepolia: process.env.ETHERSCAN_API_KEY || vars.get("ETHERSCAN_API_KEY", ""),
     },
   },
   gasReporter: {
@@ -38,6 +44,10 @@ const config: HardhatUserConfig = {
         mnemonic: MNEMONIC,
       },
       chainId: 31337,
+      mining: {
+        auto: true,
+        interval: [1000, 3000] // 在1-3秒之间随机出块
+      },
     },
     anvil: {
       accounts: {
@@ -49,7 +59,7 @@ const config: HardhatUserConfig = {
       url: "http://localhost:8545",
     },
     sepolia: {
-      accounts: {
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : {
         mnemonic: MNEMONIC,
         path: "m/44'/60'/0'/0/",
         count: 10,
@@ -79,6 +89,7 @@ const config: HardhatUserConfig = {
         runs: 800,
       },
       evmVersion: "cancun",
+      viaIR: true,  // Enable IR-based compilation to avoid stack too deep errors
     },
   },
   typechain: {
